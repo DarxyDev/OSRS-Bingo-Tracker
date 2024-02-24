539f03361e57d68afc4247d5d180e2ea0d092bd6
DarxyDev <damongarvin@gmail.com>
2024-02-24 17:10:11 -0500

    changed goblin credit to component

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,16 @@ function App() {
     sheetError: false,
     sheet: undefined,
   });
-  if (!state.sheetLoaded || state.sheetError)
-    return <SheetLoading {...{ state, setState }} />
 
-  return (
+  if (!state.sheetLoaded || state.sheetError)
+    return (
+      <>
+        <SheetLoading {...{ state, setState }} />
+      </>
+    )
+  else return (
     <>
       Under construction..
-      <div className="icon-credit"><img src="./goblin64x64.png" height="16" width="16" /><a target="_blank" href="https://icons8.com/icon/AjpYhrt5HtC7/goblin">Goblin</a><p>icon</p><p>by</p><a target="_blank" href="https://icons8.com">Icons8</a></div>
     </>
   )
 }
---
+++ src/Components/GoblinCredit.jsx
@@ -0,0 +1,11 @@
+export default function GoblinCredit() {
+    return (
+        <>
+            <div className="icon-credit"><img src="./goblin64x64.png" height="16" width="16" /><a target="_blank"
+                href="https://icons8.com/icon/AjpYhrt5HtC7/goblin">Goblin</a>
+                <p>icon</p>
+                <p>by</p><a target="_blank" href="https://icons8.com">Icons8</a>
+            </div>
+        </>
+    )
+}
