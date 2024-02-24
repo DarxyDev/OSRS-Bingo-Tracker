42381231b0b54186c27a148e8b1da09968b84f85
DarxyDev <damongarvin@gmail.com>
2024-02-24 15:44:57 -0500

    init

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './styles/reset.css'
+import './styles/App.css'
+
+function App() {
+
+  return (
+    <>
+      Under construction...
+      
+    <div class="icon-credit"><img src="./goblin64x64.png" height="16" width="16"/><a target="_blank" href="https://icons8.com/icon/AjpYhrt5HtC7/goblin">Goblin</a><p>icon</p><p>by</p><a target="_blank" href="https://icons8.com">Icons8</a></div>
+    </>
+  )
+}
+
+export default App
