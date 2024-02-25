e1eb7f14f4d090a3a48d49373272b7a332ed690a
DarxyDev <damongarvin@gmail.com>
2024-02-24 22:31:59 -0500

    implemented sheetManager.addCategory()

--- src/sheetManager.js
+++ src/sheetManager.js
@@ -30,6 +30,18 @@ export default function sheetManagerFactory({
         // 1. Load the JavaScript client library.
         gapi.load('client', loadSheet);
     }
+    function getSheet() { return sheet }
+    function addCategory(categoryName) {
+        let emptyIndex = 0;
+        for (let i = 0; i <= sheet.values.length; i++) {
+            if (sheet.values[i] === undefined) sheet.values.push([]);
+            if (sheet.values[i].length === 0 || sheet.values[i][0] === "") {
+                emptyIndex = i;
+                break;
+            }
+        }
+        sheet.values[emptyIndex][0] = categoryName;
+    }
 
-    return {id:'heheee', sheet, load};
+    return { load, getSheet, addCategory };
 }
